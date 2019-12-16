cb609ea71543488f5f93975a438045b43fcddcc0
magikitty <elina.satola@gmail.com>
2019-12-16 08:53:06 +0200

    Finish Q21.1 with Q1-3

--- 21.FunctionsAgain/Q21.1.py
+++ 21.FunctionsAgain/Q21.1.py
@@ -5,7 +5,16 @@ Get Programming: Learn to Code With Python
 
 # Q1
 def calculate_total(price, percent):
-    total = float(price) + int(percent) * float(price)
+    total = float(price) + int(percent) / float(price)
     return total
 
-print(calculate_total(10, 5))
+print(calculate_total(100, 25))
+
+# Q2
+print(calculate_total(20, 15))
+
+# Q3
+my_price = 78.55
+my_tip = 20
+new_total = calculate_total(my_price, my_tip)
+print("Your new total is " + str(new_total))
