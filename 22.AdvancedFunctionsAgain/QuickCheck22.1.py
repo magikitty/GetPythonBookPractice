c77337d8e2efdf3c82cd472f6d7bad2ead98be50
magikitty <elina.satola@gmail.com>
2019-12-17 10:26:34 +0200

    Create QuickCheck22.1.py

---
+++ 22.AdvancedFunctionsAgain/QuickCheck22.1.py
@@ -0,0 +1,17 @@
+"""
+This is Lesson 22: Advanced operations with functions
+Get Programming: Learn to Code With Python
+"""
+
+def func(a, b):
+    x = a + b
+    y = a - b
+    print(x * y)
+    return(x / y)
+
+a = 1
+b = 2
+x = 5
+y = 6
+
+print(func(x, y))
