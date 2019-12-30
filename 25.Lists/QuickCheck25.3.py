874459e637dd426dff3c20483a5739b14e997312
magikitty <elina.satola@gmail.com>
2019-12-30 13:59:36 +0200

    Modify Quick Check 25.3

--- 25.Lists/QuickCheck25.3.py
+++ 25.Lists/QuickCheck25.3.py
@@ -2,6 +2,7 @@
 This is Lesson 25: Working with lists
 Get Programming: Learn to Code With Python
 """
+# The operations count, len and index can also be performed on lists
 
 L = ["one", "three", "two", "three", "four", "three", "three", "five"]
 
