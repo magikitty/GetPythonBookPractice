2c697688b770139fb61f6fa3be5fda510a8ee10c
magikitty <elina.satola@gmail.com>
2020-01-02 14:32:04 +0200

    Edit description

--- 25.Lists/QuickCheck25.5.py
+++ 25.Lists/QuickCheck25.5.py
@@ -1,6 +1,10 @@
 """
 This is Lesson 25: Working with lists
 Get Programming: Learn to Code With Python
+
+This Quick Check is to practice the pop function, which removes an item from the list
+pop() returns the item that is removed from the list
+list_name.pop(i) removes the list element at index position i
 """
 
 pi = ["3", ".", "1", "4", "1", "5", "9"]
