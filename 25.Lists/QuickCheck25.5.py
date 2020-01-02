f2a1275ccb1fa3f600209b358bc376886c001bb6
magikitty <elina.satola@gmail.com>
2020-01-02 14:25:56 +0200

    Create Quick Check 25.5

---
+++ 25.Lists/QuickCheck25.5.py
@@ -0,0 +1,12 @@
+"""
+This is Lesson 25: Working with lists
+Get Programming: Learn to Code With Python
+"""
+
+pi = ["3", ".", "1", "4", "1", "5", "9"]
+pi.pop(1)   # removes the list element at index 1
+print(pi)
+pi.pop()    # removes the list element at the last index position
+print(pi)
+pi.pop()
+print(pi)
