1124caeccda990006bcc85c4d26eb0be51631e1f
neetfreek <jonathan.widdowson@neetfreek.net>
2020-01-23 17:53:37 +0200

    Complete Q26.1 and Q26.2

---
+++ 26.AdvancedLists/Q26.1.py
@@ -0,0 +1,11 @@
+"""
+This is Lesson 26. Advanced Operations with Lists
+Get Programming: Learn to Code with Python
+
+Practice with sorting lists and splitting strings into lists
+"""
+
+cities = "Tampere,Bangkok,Cape Town,Boston,London,Auckland"
+cities_list = cities.split(",")
+cities_list.sort()
+print(cities_list)
