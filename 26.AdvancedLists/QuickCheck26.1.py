696eb99ddf40b3ff5803778cde7f9fd0e67343f2
neetfreek <jonathan.widdowson@neetfreek.net>
2020-01-23 16:13:04 +0200

    Create and complete Quick Check 26.1

---
+++ 26.AdvancedLists/QuickCheck26.1.py
@@ -0,0 +1,15 @@
+"""
+This is Lesson 26. Advanced Operations with Lists
+Get Programming: Learn to Code with Python
+"""
+L = ["p", "r", "o", "g", "r", "a", "m", "m", "i", "n", "g"]
+L.reverse()
+print(L)
+L.sort()
+print(L)
+L.reverse()
+print(L)
+L.reverse()
+print(L)
+L.sort()
+print(L)
