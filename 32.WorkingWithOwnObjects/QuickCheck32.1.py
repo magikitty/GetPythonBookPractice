ececfb379a7d5e1eb6e760068daf6998573f8b89
magikitty <elina.satola@gmail.com>
2020-02-10 18:36:50 +0200

    Finish QuickCheck32.1

--- 32.WorkingWithOwnObjects/QuickCheck32.1.py
+++ 32.WorkingWithOwnObjects/QuickCheck32.1.py
@@ -2,3 +2,20 @@
 This is Lesson 32. Working with your own object types
 Get Programming: Learn to Code with Python
 """
+
+list_fruits = ["orange", "mango", "pear", "pineapple", "apple"]
+
+
+class Stack(object):
+    def __init__(self):
+        self.stack = []
+
+    def add_list(self, list_to_add):
+        for i in range(0, len(list_to_add)):
+            item = list_to_add[i]
+            self.stack.append(item)
+        return self.stack
+
+
+fruit_stack = Stack()
+print(fruit_stack.add_list(list_fruits))
